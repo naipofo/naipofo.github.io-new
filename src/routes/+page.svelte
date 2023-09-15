7bcdeda4080b5f142bc2f908ea71f5b541e3f1fd
NaiPofo <50967056+naipofo@users.noreply.github.com>
2023-09-15 09:55:50 +0000

    recreate original website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,93 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { links } from '$lib/socials.json';
+	import PhoneScreen from './PhoneScreen.svelte';
+</script>
+
+<h1>NaiPofo</h1>
+<div class="main-socials">
+	{#each links as { name, href, icon, nick }}
+		<a {href}>
+			<img src={'icons/' + icon} alt="" />
+			<div class="label">
+				<span class="nick">{nick}</span>
+				<span class="name">{name}</span>
+			</div>
+		</a>
+	{/each}
+</div>
+
+<PhoneScreen>
+	----hello----<br />this site is not done yet, please check it out later!
+</PhoneScreen>
+
+<style lang="scss">
+	:global(body) {
+		color: white;
+		background-color: black;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		font-family: monospace;
+	}
+
+	h1 {
+		filter: url('filters.svg#glitchy-text');
+		font: Min(20vw, 7rem) 'Times New Roman', serif;
+		margin-bottom: 0;
+	}
+	.main-socials {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		font: 1.2rem 'Roboto Mono', monospace;
+		width: Min(400px, 100vw);
+
+		a {
+			padding: 8px;
+
+			$glow: drop-shadow(0 0 6px #00ffff) drop-shadow(0 0 2px #00ffff88);
+
+			&:hover {
+				img {
+					transform: scale(1.3);
+					filter: $glow;
+				}
+
+				.label {
+					opacity: 1;
+				}
+			}
+
+			img {
+				transition: transform 0.3s;
+			}
+
+			.label {
+				text-align: center;
+				display: flex;
+				flex-direction: column;
+				position: absolute;
+				left: 0;
+				right: 0;
+				opacity: 0;
+				transition: opacity 0.3s;
+				pointer-events: none;
+				filter: $glow drop-shadow(0 0 6px #000000) drop-shadow(0 0 2px #000000);
+				color: white;
+
+				.nick {
+					font-size: 1.4rem;
+				}
+
+				.name {
+					font-size: 1rem;
+				}
+			}
+		}
+
+		img {
+			width: 48px;
+			height: 48px;
+		}
+	}
+</style>
---
+++ src/routes/PhoneScreen.svelte
@@ -0,0 +1,24 @@
+<div>
+    <slot />
+</div>
+
+<style>
+    div {
+		border-image: url('/phone-frame.svg');
+		border-style: solid;
+		border-image-width: 27px 42px 27px 43px;
+		border-image-outset: 27px 42px 27px 43px;
+		border-image-slice: 37 51 37 53 fill;
+		border-image-repeat: repeat;
+		width: 300px;
+		height: 120px;
+		overflow: hidden;
+		padding: 27px 50px 27px 43px;
+		margin: 3rem;
+		font-family: monospace;
+		color: #242815;
+		word-break: break-all;
+		padding: 0.8rem;
+		border-radius: 3px;
+	}
+</style>
